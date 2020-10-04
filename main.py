a983651e2b5906675f5da23a04495b027bb4ad30
Fernando Posser Pinheiro <fernando.posser@hotmail.com>
2020-10-04 19:52:44 -0300

    +

--- main.py
+++ main.py
@@ -1,4 +1,15 @@
+import os, sys
 
-with open("E:\imed-cc-so-arquivos\Pasta2\\arq6.txt", 'r') as file:
-    print("caminho: ", file.name)
-    print(file.read())
+
+def func1():
+    with open("E:\imed-cc-so-arquivos\Pasta2\\arq6.txt", 'r') as file:
+        print("caminho: ", file.name)
+        print(file.read())
+
+
+print(os.getcwd())
+
+opcao = int(input("opcao: "))
+
+if opcao == 1:
+    func1()
